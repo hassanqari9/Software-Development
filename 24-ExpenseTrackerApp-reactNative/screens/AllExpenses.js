3a334358e13ae11e221655f7af5cd1cfbfdc44d3
Hasaan Qari <hasaanqari9@Hasaans-MacBook-Air.local>
2024-01-25 02:40:41 +0530

    completed expensetracker

---
+++ 24-ExpenseTrackerApp-reactNative/screens/AllExpenses.js
@@ -0,0 +1,12 @@
+import {Text} from 'react-native'
+import { useContext } from 'react'
+
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
+import { ExpensesContext } from '../store/expenses-context'
+
+const AllExpenses = () => {
+  const expensesCtx = useContext(ExpensesContext)
+  return <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" fallbackText="No registered expenses found"/>
+}
+
+export default AllExpenses
---
+++ 24-ExpenseTrackerApp-reactNative/screens/RecentExpenses.js
@@ -0,0 +1,46 @@
+import { useContext, useEffect, useState } from 'react'
+
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
+import { ExpensesContext } from '../store/expenses-context'
+import { getDateMinusDays } from '../util/date'
+import { fetchExpenses } from '../util/http'
+import LoadingOverlay from '../components/UI/LoadingOverlay'
+import ErrorOverlay from '../components/UI/ErrorOverlay'
+
+const RecentExpenses = () => {
+  const [isFetching, setIsFetching] = useState(true)
+  const expensesCtx = useContext(ExpensesContext)
+  const [error, setError] = useState()
+
+  useEffect(() => {
+    async function getExpenses() {
+      setIsFetching(true)
+      try {
+        const expenses = await fetchExpenses();
+        expensesCtx.setExpenses(expenses)
+      } catch (error) {
+        setError('Could not fetch expenses')
+      }
+      setIsFetching(false)
+    }
+    getExpenses()
+  }, [])
+
+  if (error) {
+    return <ErrorOverlay message={error} onConfirm={() => setError(null)} />
+  }
+
+  if (isFetching) {
+    return <LoadingOverlay />
+  }
+
+  const recentExpenses = expensesCtx.expenses.filter(expense => {
+    const today = new Date()
+    const  days7DaysAgo = getDateMinusDays(today, 7)
+    return (expense.date >= days7DaysAgo) && (expense.date <= today)
+  })
+
+  return <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 Days" fallbackText='No expenses regisgered for the last 7 days'/>
+}
+
+export default RecentExpenses
